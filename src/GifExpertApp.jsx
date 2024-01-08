b78681b864be95fd03c09eabd51bbe58e78e38bf
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2024-01-08 07:14:28 -0500

    Se le adecua los estilos primarios y se crea componente para el input de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory"
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(["dragon ball"])
+
+  const onAddCategory = () => {
+    setCategories([...categories, "valorant"])
+  }
+
   return (
-    <h1>GifExpertApp</h1>
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory />
+
+      <button onClick={onAddCategory}>Agregar</button>
+
+      <ol>
+        {categories.map(category => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
+    </>
+
   )
 }
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+export const AddCategory = () => {
+
+  const [inputValue, setInputValue] = useState('Valorant')
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value)
+  }
+
+  return (
+    <form>
+      <input
+        type='text'
+        placeholder='Buscar gifs'
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+
+  )
+}
